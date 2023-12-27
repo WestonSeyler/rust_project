2f97ad6d9e31d43d748964e123d00225b1987568
wujianing <60140118+wawa0584@users.noreply.github.com>
2023-12-27 22:29:42 +0800

    feat: hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#![allow(unused)]
+
+use std::io;
+use std::io::{Write, BufRead, BufReader,ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
-}
+    println!("what is name");
+    let mut name:String = String::new();
+    let greeting:&str="Nice to meet you";
+    io::stdin().read_line(&mut name)
+        .expect("Failed to read line");
+    println!("Hello, {}! {}",name.trim_end() ,greeting);
+} 
